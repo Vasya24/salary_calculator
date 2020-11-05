fa2960df5e40229fe23ac0b8fdcd106aa66e1a06
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-11-05 10:44:56 +0300

    Unnecessary strings have been removed

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,12 @@
 const grossInput = document.getElementById('gross_input');
 const grossRes = document.querySelector('.gross_result-span');
 const netInput = document.getElementById('net_input');
-const netRes = document.querySelector('.net_result-span')
+const netRes = document.querySelector('.net_result-span');
 
 grossInput.oninput = () => {
   grossRes.innerHTML = Math.ceil(((grossInput.value / 87) * 100));
-  if (grossRes.innerHTML === NaN) {
-  }
-}
+};
 
 netInput.oninput = () => {
-  netRes.innerHTML = Math.ceil(((netInput.value / 100) * 87))
-}
+  netRes.innerHTML = Math.ceil(((netInput.value / 100) * 87));
+};
