f453cdc84b626d5cc642ecacac966340c9f48eaf
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-11-05 10:37:35 +0300

    Done

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+const grossInput = document.getElementById('gross_input');
+const grossRes = document.querySelector('.gross_result-span');
+const netInput = document.getElementById('net_input');
+const netRes = document.querySelector('.net_result-span')
+
+grossInput.oninput = () => {
+  grossRes.innerHTML = Math.ceil(((grossInput.value / 87) * 100));
+  if (grossRes.innerHTML === NaN) {
+  }
+}
+
+netInput.oninput = () => {
+  netRes.innerHTML = Math.ceil(((netInput.value / 100) * 87))
+}
